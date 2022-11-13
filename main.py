312dd7233b53d411bcbe5d9c07e6d6c795ef54de
Tyler Nguyen <123tylernguyen@gmail.com>
2022-11-12 20:15:49 -0500

    added routing to pages

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from final import create_app #,render_template
 
 app = create_app()
 
-#@app.route("home")
-#def home():
-#    return render_template("home.html")
+@app.route("home")
+def home():
+    return render_template("home.html")
 
+@app.route("login")
+def home():
+    return render_template("login.html")
+
+@app.route("profile")
+def home():
+    return render_template("profile.html")
+
+@app.route("logout")
+def logout():
+    return render_template("home.html")
+
+@app.route("diabetes")
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
